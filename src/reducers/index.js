552986928fc6d6bdb06e9b33fa8b5a74c091498e
Robin Louis van Komen <robin@vkmedia.dk>
2019-04-12 18:59:25 +0200

    add redux form

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux'
+import { reducer as reduxFormReducer } from 'redux-form'
 
 import userReducer from './userReducer'
 import loadReducer from './loadReducer'
 
 export default combineReducers({
     user: userReducer,
-    isLoading: loadReducer
+    isLoading: loadReducer,
+    form: reduxFormReducer
 })
